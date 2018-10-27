e5b735d37ad0e61350e429cd19d74b9cb4266342
sasaki819 <sasaki819@gmail.com>
2018-10-28 01:27:56 +0900

    delte counter controller(because it' s not necessary.

--- board/client/counter/counter.js
+++ board/client/counter/counter.js
@@ -2,15 +2,6 @@ import counterTemplate from "./counter.html";
 
 angular.module("app").component("counter", {
     templateUrl: counterTemplate,
-    controller: function () {
-        const ctrl = this;
-        ctrl.countUp = function () {
-            ctrl.onCountUp();
-        };
-        ctrl.countDown = function () {
-            ctrl.onCountDown();
-        };
-    },
     bindings: {
         count: "=",
         unit: "<",
