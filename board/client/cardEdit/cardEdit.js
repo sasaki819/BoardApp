bc1a6b36142ec054a9facd8495d19dc90a93ad84
sasaki819 <sasaki819@gmail.com>
2018-11-04 07:32:11 +0000

    imprement delete/undelete function with comfirm dialog on cardEdit

--- board/client/cardEdit/cardEdit.js
+++ board/client/cardEdit/cardEdit.js
@@ -5,18 +5,37 @@ angular.module("app").controller("cardEditController", function ($scope, $mdDial
     }
     $scope.card.cardTag = ['toilet'];
     $scope.showConfirm = function (ev) {
-        var confirm = $mdDialog.confirm()
-            .title('Confirmation')
-            .textContent('Are you sure you delete this card?')
-            .targetEvent(ev)
-            .ok('Please do it!')
-            .cancel('cancel!');
-
-        $mdDialog.show(confirm).then(function () {
-            $scope.onClickDelete();
-        }, function () {
-            
-        });
+        console.log("showConfirm", $scope.card.deleted);
+        if ($scope.card.deleted === false) {
+            const confirm = $mdDialog.confirm()
+                .title('Confirmation')
+                .textContent('Are you sure you delete this card?')
+                .targetEvent(ev)
+                .ok('OK')
+                .cancel('Cancel');
+            confirm._options.multiple = true;
+            $mdDialog.show(confirm).then(function () {
+                Meteor.call("cards.update", $scope.card._id, { deleted: true });
+                $mdDialog.hide();
+            }, function () {
+                //「キャンセル」押した場合の処理なし
+            });
+            //deletedがtrueの場合
+        } else {
+            const confirm = $mdDialog.confirm()
+                .title('Confirmation')
+                .textContent('Are you sure you undelete this card?')
+                .targetEvent(ev)
+                .ok('OK')
+                .cancel('Cancel');
+            confirm._options.multiple = true;
+            $mdDialog.show(confirm).then(function () {
+                Meteor.call("cards.update", $scope.card._id, { deleted: false });
+                $mdDialog.hide();
+            }, function () {
+                //「キャンセル」押した場合の処理なし
+            });
+        }
     };
     $scope.onClickCountUp = function () {
         $scope.card.count++;
@@ -24,24 +43,11 @@ angular.module("app").controller("cardEditController", function ($scope, $mdDial
     $scope.onClickCountDown = function () {
         $scope.card.count--;
     };
-    $scope.onClickDelete = function () {
-        console.log("onClickDelete called.", $scope.card._id);
-        Meteor.call("cards.update", $scope.card._id, {deleted: true});
-        $mdDialog.hide();
-    };
 
     $scope.close = $mdDialog.cancel;
 
     $scope.updateCard = function () {
         Meteor.call("cards.update", $scope.card._id, $scope.card);
         $mdDialog.hide();
-    };
-
-    $scope.displayCounter = function () {
-        if ($scope.type == "Stock") {
-            return true;
-        } else {
-            return false;
-        }
     };
 });
